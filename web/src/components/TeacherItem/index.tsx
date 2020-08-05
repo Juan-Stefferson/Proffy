ae74a85713a4ad1d8ea31a4c1bffa95aca40c416
root <juanpereira74@hotmail.com>
2020-08-04 21:36:21 -0300

    added prettier

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,38 +1,41 @@
-import React from 'react';
+import React from "react";
 
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import "./styles.css";
 
-import './styles.css'
-
-function TeacherItem(){
-    return(
-
-        <article className="teacher-item">
-        <header>
-            <img src="https://avatars3.githubusercontent.com/u/36244501?s=460&u=156e06f66fe491e8a71385c0989437cfb6f7d7c7&v=4" alt="Juan Pereira"/>
-            <div>
-                <strong>Juan Pereira</strong>
-                <span>Programação</span>
-            </div>
-        </header>
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/36244501?s=460&u=156e06f66fe491e8a71385c0989437cfb6f7d7c7&v=4"
+          alt="Juan Pereira"
+        />
+        <div>
+          <strong>Juan Pereira</strong>
+          <span>Programação</span>
+        </div>
+      </header>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
+        architecto necessitatibus.
+        <br /> <br />
+        Autem distinctio nisi aperiam neque unde maiores sed expedita illo, eos
+        sunt consequatur vitae, velit temporibus, odit quia quibusdam.
+      </p>
+      <footer>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, architecto necessitatibus. 
-            <br/> <br/>
-            Autem distinctio nisi aperiam neque unde maiores sed expedita illo, eos sunt consequatur vitae, velit temporibus, odit quia quibusdam.
+          Preço/hora
+          <strong>R$ 40,00</strong>
         </p>
-        <footer>
-            <p>
-                Preço/hora
-                <strong>R$ 40,00</strong>
-            </p>
-            <button type ="button">
-                <img src={whatsappIcon} alt="Whatsapp"/>
-                Entrar em contato!
-            </button>
-        </footer>
-        </article>
-    );
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato!
+        </button>
+      </footer>
+    </article>
+  );
 }
 
-export default TeacherItem ;
+export default TeacherItem;
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import PageHeader from '../../components/PageHeader';
-import './styles.css';
+import PageHeader from "../../components/PageHeader";
+import "./styles.css";
 
-
-
-
-function TeacherForm(){
-    return(
+function TeacherForm() {
+  return (
     <div id="page-teacher-form" className="container">
-    <PageHeader title ="Que incrivel, que voce quer dar aulas!! "/>
-</div>    
-)
+      <PageHeader title="Que incrivel, que voce quer dar aulas!! " />
+    </div>
+  );
 }
 
 export default TeacherForm;
